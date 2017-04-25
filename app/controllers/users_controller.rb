c494cdfeaf0b4297d1fe996f9a05b3baa7a5af98
Jasmine Ellis <ellis.jas48@gmail.com>
2017-04-25 06:51:17 +0000

    Fix type for views/shared

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,13 @@ class UsersController < ApplicationController
      else
        render 'new'
      end
+  end
+
+  private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
    end
-
-   private
-
-     def user_params
-       params.require(:user).permit(:name, :email, :password,
-                                    :password_confirmation)
-     end
- end
+   
+end
