ba6b6e30ac3e99f07e51b77e9ea5d1330afb802b
Haotian Xu <s3374213@student.rmit.edu.au>
2017-04-27 00:27:25 +1000

    fix password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50, minimum: 4 }
   VALID_EMAIL_REGEX = /\A[A-z]+\.+[A-z]+@rmit.edu.au\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  has_secure_password
   VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W]).{8,}\z/
   validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }
   
-  has_secure_password
+
 end
