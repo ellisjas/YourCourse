1de4d0bdcacc41661d2c1b2646f0b0d83e88ce02
Haotian Xu <s3374213@student.rmit.edu.au>
2017-04-27 23:35:31 +1000

    Login after register

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,8 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # if user loged in, return true
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
