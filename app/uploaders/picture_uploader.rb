78a27d2d0e310a97b3c7c1d147926cbce0650c22
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-06 08:57:29 +0000

    Add tabs and list of coordinators to courses index

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,14 +3,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [200, 200]
-
-  # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
+  process resize_to_limit: [500, 500]
+  
+  version :thumb do
+    process resize_to_fill: [200,200]
   end
+  
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -31,11 +30,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   #
   # def scale(width, height)
   #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
