80880b336015e18afc8a74190509795c61e436cf
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-06 13:55:17 +0000

    Add user form validation messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 50, minimum: 4 }
   VALID_EMAIL_REGEX = /\A[A-z]+\.+[A-z]+@rmit.edu.au\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX, 
+                                                                        message: "must be in the format <first-name>.<last.name>@rmit.edu.au" }, 
+                                                                        uniqueness: { case_sensitive: false }
   
   has_secure_password
   VALID_PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W]).{8,}\z/
-  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: VALID_PASSWORD_REGEX, 
+                                                                          message: "must contain at least one capital letter, a number, and a special character." }
 
   has_secure_password
   
