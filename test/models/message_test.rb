4fecc9437bbce4a48765508e4435fcc0c5b5b5b7
Jasmine Ellis <ellis.jas48@gmail.com>
2017-05-11 02:55:47 +0000

    Add message model and controller

---
+++ test/models/message_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class MessageTest < ActiveSupport::TestCase
+  
+  test 'responds to name, email and body' do 
+    msg = Message.new
+
+    assert msg.respond_to?(:name),  'does not respond to :name'
+    assert msg.respond_to?(:email), 'does not respond to :email'
+    assert msg.respond_to?(:body),  'does not respond to :body'
+  end
+  
+  test 'should be valid when all attributes are set' do
+    attrs = { 
+      name: 'stephen',
+      email: 'stephen@example.org',
+      body: 'kthnxbai'
+    }
+
+    msg = Message.new attrs
+    assert msg.valid?, 'should be valid'
+  end
+  
+  test 'name, email and body are required by law' do
+    msg = Message.new
+
+    assert_not msg.valid?, 'Blank Message should be invalid'
+  end
+  
+end
